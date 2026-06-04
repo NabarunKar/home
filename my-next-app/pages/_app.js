449512827cb083630fae38ceaa121cf7fb54473b
NabarunKar <nabarunkar01@gmail.com>
2026-06-03 20:10:02 -0500

    resume changes

--- my-next-app/pages/_app.js
+++ my-next-app/pages/_app.js
@@ -1,5 +1,14 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/man.png" type="image/png" />
+        <link rel="shortcut icon" href="/man.png" type="image/png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
